be48ec623ece77410381089dc5f067a2654cde6f
sbooker <sbooker@mail.ru>
2019-12-09 23:16:57 +0300

    Update for swift 5.1

--- Package.swift
+++ Package.swift
@@ -1,9 +1,19 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
-    name: "Nifty-libs",
-    providers: [
-        .Apt("gfortran liblapack3 liblapacke liblapacke-dev libopenblas-base libopenblas-dev"),
-        .Brew("homebrew/dupes/lapack homebrew/science/openblas")
+    name: "Nifty-libs",  
+    products: [
+        .library(name: "Nifty-libs", targets: ["Nifty-libs"]),
+    ],
+    targets: [
+        .systemLibrary(
+            name: "Nifty-libs",
+            providers: [
+                .apt(["gfortran", "liblapack3", "liblapacke", "liblapacke-dev", "libopenblas-base", "libopenblas-dev"]),
+                .brew(["homebrew/dupes/lapack", "homebrew/science/openblas"])
+            ]
+        )
     ]
 )
